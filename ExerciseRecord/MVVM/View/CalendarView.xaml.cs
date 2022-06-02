ebd0ac2c425f8d6ae0e310bfe629a61286821612
dawon <six29@naver.com>
2022-06-02 18:12:43 +0900

    20220602

--- ExerciseRecord/MVVM/View/CalendarView.xaml.cs
+++ ExerciseRecord/MVVM/View/CalendarView.xaml.cs
@@ -12,18 +12,43 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
 
 namespace ExerciseRecord.MVVM.View
 {
-    /// <summary>
-    /// CalendarView.xaml에 대한 상호 작용 논리
-    /// </summary>
+    
     public partial class CalendarView : UserControl
     {
+        private const string BasePath = "https://dcdc-14c7d-default-rtdb.firebaseio.com/";//본인의 FB URL
+        private const string FirebaseSecret = "e54zPLcujgDI9A9LflL9zVzrgyJIWbjbfT5bszzC"; // FB 비번
+        private static FirebaseClient _client;
+
         public CalendarView()
         {
             InitializeComponent();
+            IFirebaseConfig config = new FirebaseConfig
+            {
+                AuthSecret = FirebaseSecret,
+                BasePath = BasePath
+            };
+            _client = new FirebaseClient(config);
         }
 
+        private async void Calendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //캘린더 날짜 
+            var date = sender as Calendar;
+            string td = date.SelectedDate.ToString();
+            string ttdd = td.Substring(0, 10);
+            ttdd = ttdd.Replace("-", "");
+
+            //날짜에 해당하는 데이터
+            FirebaseResponse response = await _client.GetAsync(ttdd + "/LR");
+            int value = response.ResultAs<int>();
+            MessageBox.Show(value.ToString());
+        }
     }
 }
--- ExerciseRecord/MVVM/View/TodayView.xaml.cs
+++ ExerciseRecord/MVVM/View/TodayView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
 
 namespace ExerciseRecord.MVVM.View
 {
@@ -20,10 +26,42 @@ namespace ExerciseRecord.MVVM.View
     /// </summary>
     public partial class TodayView : UserControl
     {
+        private const string BasePath = "https://dcdc-14c7d-default-rtdb.firebaseio.com/";//본인의 FB URL
+        private const string FirebaseSecret = "e54zPLcujgDI9A9LflL9zVzrgyJIWbjbfT5bszzC"; // FB 비번
+        private static FirebaseClient _client;
+        public string today;
+
+        string connStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\dawon\source\repos\ExerciseRecord\ExerciseRecord\Memo.mdf;Integrated Security=True";
+        DispatcherTimer t = new DispatcherTimer();
+
         public TodayView()
         {
             InitializeComponent();
+
+            IFirebaseConfig config = new FirebaseConfig
+            {
+                AuthSecret = FirebaseSecret,
+                BasePath = BasePath
+            };
+            _client = new FirebaseClient(config);
+
+            DateTime datetime = DateTime.Now;
+            today = datetime.ToString();
+            today = today.Substring(0, 10);
+            today = today.Replace("-", "");
+
+            getfb();
+
+            t.Interval = new TimeSpan(0, 0, 1); //1초
         }
+
+        private async void getfb()
+        {
+            FirebaseResponse response = await _client.GetAsync(today + "/LR");
+            int value = response.ResultAs<int>();
+            MessageBox.Show(value.ToString());
+        }
+
         List<Exercise> items = new List<Exercise>();
         public class Exercise
         {
@@ -35,7 +73,17 @@ namespace ExerciseRecord.MVVM.View
 
         private void btnMemo_Click(object sender, RoutedEventArgs e)
         {
-           
+            SqlConnection conn = new SqlConnection(connStr);
+            string date = DateTime.Now.ToString("yyyyMMdd");
+
+            string sqld = string.Format("SELECT COUNT(*) From MemoTable WHERE Date='{0}'", date);
+            if()
+
+
+            string sql = string.Format("INSERT INTO MemoTable Values ('{0}', '{1}')", date, txtTodayMemo.Text);
+            MessageBox.Show(sql);
+
+            
         }
     }
 }
